daa06d2e99890cdfa649c24555e052acc54cc121
mwilbz <mattwilber94@gmail.com>
2020-04-24 00:11:52 -0700

    Add contriubtion information and setup.py

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# tractor
-The chinese card game
+# Tractor
+
+Tractor is a Chinese card cade, also known as [Sheng Ji](https://en.wikipedia.org/wiki/Sheng_ji).
+
+## Contributing
+
+### Environment Setup
+
+Make sure you have Python 3.8 installed (I suggest using [pyenv](https://github.com/pyenv/pyenv#installation)). After cloning this repository, create a virtual environment with
+
+```
+python -m venv venv
+source venv/bin/activate
+pip install -e .
+```
+
+in the project directory.
+
+### Testing
+
+Run tests with the `pytest` command in the project directory.
---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tractor", # Replace with your own username
+    version="0.0.1",
+    author="Matt Wilber",
+    description="Tractor card game implementation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mwilbz/tractor",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        'pytest'
+    ]
+)
